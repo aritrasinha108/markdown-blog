8cd638f9a7454c0af49e2f589c22b1b50d80769e
Aritra Sinha <55765656+aritrasinha108@users.noreply.github.com>
2020-07-30 19:20:03 +0530

    The basic crud properties
    It has basic crud properties for the blog

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const articleRouter = require('./routes/article');
+const Article = require('./model/article');
+const mongoose = require('mongoose');
+const methoddOverride = require('method-override');
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+app.use(methoddOverride('_method'));
+app.use(express.urlencoded({ extended: false }));
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+    let articles = await Article.find().sort({ created: 'desc' });
+    res.render('articles/index', { articles: articles });
+});
+app.use('/articles', articleRouter);
+app.listen(80);
---
+++ routes/article.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const Article = require('../model/article');
+
+
+router.get('/new', (req, res) => {
+    res.render('articles/new', { article: new Article() });
+});
+
+
+router.post('/', async (req, res, next) => {
+
+    req.article = new Article();
+    next();
+
+}, saveArticleAndRedirect('new'));
+
+
+router.put('/:id', async (req, res, next) => {
+
+    req.article = await Article.findById(req.params.id);
+    next();
+
+}, saveArticleAndRedirect('edit'));
+
+
+router.get('/:slug', async (req, res) => {
+    const article = await Article.findOne({ slug: req.params.slug });
+    if (article == null)
+        res.redirect('/');
+    res.render('articles/show', { article: article });
+});
+
+router.get('/edit/:id', async (req, res) => {
+    let article = await Article.findById(req.params.id);
+    res.render('articles/edit', { article: article });
+});
+
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/');
+});
+
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article;
+        article.title = req.body.title;
+        article.description = req.body.description;
+        article.markdown = req.body.markdown;
+        try {
+            article = await article.save();
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            res.render(`articles/${path}`, { article: article });
+            console.log(e);
+        }
+    }
+}
+
+
+module.exports = router;
